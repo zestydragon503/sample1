4c2f357ee9bc0d21ec45670cc63bedf4afa55209
roopa <roopa@splitchennai.com>
2020-07-28 14:12:25 +0530

    new page created

---
+++ src/pages/Login.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <>
+        <div className="container">
+          <div className="col">
+            <div className="row">
+              <div className="col-2">Hello</div>
+              <div className="col">Hiiii</div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Login;
